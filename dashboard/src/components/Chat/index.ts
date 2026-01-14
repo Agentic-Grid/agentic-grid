68c12acda407283262208f4e301fe6d7adcaf3fe
digoocorrea <digoocorrea@gmail.com>
2026-01-14 16:07:30 -0300

    Delete dashboard directory

--- dashboard/src/components/Chat/index.ts
+++
@@ -1,6 +0,0 @@
-export { ChatView } from "./ChatView";
-export { MessageBubble } from "./MessageBubble";
-export { ToolCallCard } from "./ToolCallCard";
-export { ChatInput } from "./ChatInput";
-export { CommandPicker } from "./CommandPicker";
-export { ApprovalCard } from "./ApprovalCard";
